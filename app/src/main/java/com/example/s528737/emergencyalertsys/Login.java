5ae485b6e519fc82c06645f7a45ac6e369c5c73f
TilakDhulipalla <s528817@mail.nwmissouri.edu>
2017-10-06 19:14:42 -0500

    added forgot page

--- app/src/main/java/com/example/s528737/emergencyalertsys/Login.java
+++ app/src/main/java/com/example/s528737/emergencyalertsys/Login.java
@@ -1,7 +1,9 @@
 package com.example.s528737.emergencyalertsys;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,4 +12,17 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+    public void toAlert(View v){
+        Intent intentalert = new Intent(this,AlertPage.class);
+        startActivity(intentalert);
+    }
+    public void toRegister(View v){
+        Intent intentregister =new Intent(this, Register.class);
+        startActivity(intentregister);
+    }
+    public void toForgot(View v){
+        Intent intentforgot = new Intent(this, ForgotPassword.class);
+        startActivity(intentforgot);
+    }
+
 }
