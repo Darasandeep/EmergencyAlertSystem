063727d11c5d176ff6ec0353092e0ba5b6b67659
raghavender jarupati <raghavender.jarupati@mail.nwmissouri.edu>
2017-10-06 20:46:02 -0500

    Added password reset

--- app/src/main/java/com/example/s528737/emergencyalertsys/ForgotPassword.java
+++ app/src/main/java/com/example/s528737/emergencyalertsys/ForgotPassword.java
@@ -2,6 +2,8 @@ package com.example.s528737.emergencyalertsys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ForgotPassword extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class ForgotPassword extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
     }
+
+    public void textView(View v)
+    {
+        TextView tv = (TextView) findViewById(R.id.textView12);
+        tv.setText("reset link sent successfully");
+    }
+
 }
