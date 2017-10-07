c1bbfae9df8b272be4ffe1ea6d2b692147ce65b8
TilakDhulipalla <s528817@mail.nwmissouri.edu>
2017-10-06 20:05:42 -0500

    contacts

--- app/src/main/java/com/example/s528737/emergencyalertsys/AlertPage.java
+++ app/src/main/java/com/example/s528737/emergencyalertsys/AlertPage.java
@@ -1,7 +1,10 @@
 package com.example.s528737.emergencyalertsys;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AlertPage extends AppCompatActivity {
 
@@ -10,4 +13,11 @@ public class AlertPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alert_page);
     }
+    public void Implicit(View v){
+
+
+                Intent intent= new Intent(Intent.ACTION_DEFAULT,  ContactsContract.Contacts.CONTENT_URI);
+
+                startActivityForResult(intent, 1);
+            }
 }
