5cce508c0073e36408d10044dc0432d238414b93
raghavender jarupati <raghavender.jarupati@mail.nwmissouri.edu>
2017-11-16 13:50:32 -0600

    Added menu bar

--- app/src/main/java/com/example/s528737/emergencyalertsys/AppContacts.java
+++ app/src/main/java/com/example/s528737/emergencyalertsys/AppContacts.java
@@ -1,6 +1,9 @@
 package com.example.s528737.emergencyalertsys;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,12 +16,13 @@ public class AppContacts extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_contacts);
     }
-
+    static final int PICK_CONTACT=1;
 public void Implicit(View v)
 {
-                Intent intent= new Intent(Intent.ACTION_DEFAULT,  ContactsContract.Contacts.CONTENT_URI);
 
-              startActivityForResult(intent, 1);
+
+    Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+    startActivityForResult(intent, PICK_CONTACT);
 }
 
     public void groupManger(View v)
@@ -26,5 +30,40 @@ public void Implicit(View v)
         Intent intent= new Intent(this, ContactGroups.class);
 
         startActivity(intent);
+    }
+
+
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+        switch (reqCode) {
+            case (PICK_CONTACT) :
+                if (resultCode == Activity.RESULT_OK) {
+
+                    Uri contactData = data.getData();
+                    Cursor c =  managedQuery(contactData, null, null, null, null);
+                    if (c.moveToFirst()) {
+
+
+                        String id =c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
+
+                        String hasPhone =c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+
+                        if (hasPhone.equalsIgnoreCase("1")) {
+                            Cursor phones = getContentResolver().query(
+                                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,
+                                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = "+ id,
+                                    null, null);
+                            phones.moveToFirst();
+                            String cNumber = phones.getString(phones.getColumnIndex("data1"));
+                            System.out.println("number is:"+cNumber);
+                        }
+                        String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+
+                    }
+                }
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/s528737/emergencyalertsys/Login.java
+++ app/src/main/java/com/example/s528737/emergencyalertsys/Login.java
@@ -1,17 +1,32 @@
+//Raghavender Jarupati
+
 package com.example.s528737.emergencyalertsys;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
 
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+        setSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setSupportActionBar().setDisplayShowHomeEnabled(true);
+        setSupportActionBar().setDisplayUseLogoEnabled(true);
+        setSupportActionBar().setLogo(R.drawable.home);
+
         setContentView(R.layout.activity_login);
     }
+
+
+
     public void toAlert(View v){
         Intent intentalert = new Intent(this,AlertPage.class);
         startActivity(intentalert);
